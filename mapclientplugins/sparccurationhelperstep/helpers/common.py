e491432a34ae8e5c379af2333834468ef2400838
hsorby <h.sorby@auckland.ac.nz>
2022-03-14 23:54:14 +1300

    Improve detection of dataset root directory.

--- mapclientplugins/sparccurationhelperstep/helpers/common.py
+++ mapclientplugins/sparccurationhelperstep/helpers/common.py
@@ -10,7 +10,24 @@ def relative_to_dataset_dir(path):
     We are determining the root directory by searching for the first `files(/|\\)`
     entry, and saying that that is the dataset root directory.
 
+    If `files` is not found we look for `derivative` or `primary` instead.
+    If found, we use the parent directory of the directory found as the
+    dataset root directory.
+
+    If nothing is found we return None.
+
     :param path: Path to work with, str.
     :return: Relative path from dataset root directory.
     """
-    return re.sub(r'^.*?files' + os.sep, 'files' + os.sep, path)
+    m = re.match(r'(^.*files[\\/])', path)
+    if m is not None:
+        return os.path.relpath(path, m.group(1))
+
+    sds_directories_regexp = [r'(^.*derivative[\\/])', r'(^.*primary[\\/])']
+    for sds_directory_regexp in sds_directories_regexp:
+        m = re.match(sds_directory_regexp, path)
+        if m is not None:
+            dataset_dir = os.path.dirname(os.path.dirname(m.group(1)))
+            return os.path.relpath(path, dataset_dir)
+
+    return None
