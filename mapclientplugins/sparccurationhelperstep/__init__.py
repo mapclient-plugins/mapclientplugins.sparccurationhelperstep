64c8f61dd6bafd4c085e72b12a6f7bd363681b9e
hsorby <h.sorby@auckland.ac.nz>
2022-03-17 10:39:34 +1300

    Set version to 0.3.1.

--- mapclientplugins/sparccurationhelperstep/__init__.py
+++ mapclientplugins/sparccurationhelperstep/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Kay Wang'
 __stepname__ = 'Sparc Curation Helper'
 __location__ = 'https://github.com/mapclient-plugins/mapclientplugins.sparccurationhelperstep'
